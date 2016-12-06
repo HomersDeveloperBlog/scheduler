d289c9d8598e580e81b1c3b6317243cb5c45f9ac
HomersDeveloperBlog <jordanthomer@gmail.com>
2016-12-06 15:22:06 -0500

    Initial commit
    Notes on the problem in an empty solution file.

---
+++ Classroom Scheduler/Classroom Scheduler/Main.cpp
@@ -0,0 +1,21 @@
+
+//R classrooms
+//K time slots
+//N courses
+//M students, each taking courses
+//Students as a list of lists of course numbers
+//
+//We require an answer: for each course, {room, time slot} = {r,k} 
+
+//A constraint graph representing the courses is created,
+//For each student's course list, the pairs of courses are enumerated.
+//For each pair, an edge between two courses is added to the constraint graph 
+//We attempt to K-colour the graph, such that there are no more than R nodes of each colour
+//If successful, the r^th node of colour k is scheduled at {r,k}
+
+int main()
+{
+	
+	
+	return 0;
+}
